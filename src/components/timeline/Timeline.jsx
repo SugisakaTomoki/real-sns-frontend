5dde5341b60ad7323bf3cd002bf86ddeb9766977
TKG0514 <highjumptomoki.0514@gmail.com>
2023-12-04 22:18:11 +0900

    ダミーデータからPost情報取得

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import "./Timeline.css";
 import { Share } from "../share/Share";
 import { Post } from "../post/Post";
+import { Posts } from "../../dummyData";
 
 const Timeline = () => {
   return (
     <div className="timeline">
       <div className="timelineWrapper">
         <Share />
-        <Post />
+        {/* /* Posts配列をマップして、各postに対して以下の処理を行う */}
+        {Posts.map((post) => (
+          // Postコンポーネントを呼び出して、現在のpostを渡す
+          <Post post={post} key={post.id} />
+        ))}
       </div>
     </div>
   );
