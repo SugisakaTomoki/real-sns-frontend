a00a2d765a8fc2b138a56489f140d5f8d9c5a215
TKG0514 <highjumptomoki.0514@gmail.com>
2023-12-04 21:38:45 +0900

    component追加

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./Timeline.css";
+import { Share } from "../share/Share";
+import { Post } from "../post/Post";
 
 const Timeline = () => {
-  return <div>Timeline</div>;
+  return (
+    <div className="timeline">
+      <div className="timelineWrapper">
+        <Share />
+        <Post />
+      </div>
+    </div>
+  );
 };
 
 export default Timeline;
